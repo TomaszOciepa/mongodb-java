711cb4c11688b046ca865bd897f31ada450f8cae
tomaszociepa <tom.ociepa@gmail.com>
2019-11-06 18:23:26 +0100

    create read method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,25 @@ public class Main {
         MongoDatabase mongoDatabase = mongoClient.getDatabase("vehicles"); //db
         MongoCollection mongoCollection = mongoDatabase.getCollection("cars"); //collection - tabela
 
+//        save(mongoCollection);
+        read(mongoCollection);
+        readByParam(mongoCollection, "Mark", "SUBARU");
+    }
 
+    private static void readByParam(MongoCollection mongoCollection, String param, Object value) {
+        Document document = new Document();
+        document.put(param, value);
+        Document first = (Document) mongoCollection.find(document).first();
+        System.out.println(first.toJson());
+    }
+
+    private static void read(MongoCollection mongoCollection) {
+
+        Document first = (Document) mongoCollection.find().first();
+        System.out.println(first.toJson());
+    }
+
+    private static void save(MongoCollection mongoCollection) {
         Document document1 = new Document();
         document1.put("Mark", "Fiat");
         document1.put("Model", "126p");
@@ -23,6 +41,11 @@ public class Main {
         document2.put("Model", "X5");
         document2.put("Color", "Black");
 
-        mongoCollection.insertMany(Arrays.asList(document1, document2));
+        Document document3 = new Document();
+        document3.put("Mark", "SUBARU");
+        document3.put("Model", "WRX");
+        document3.put("Color", Arrays.asList("blue", "green", "black"));
+
+        mongoCollection.insertMany(Arrays.asList(document1, document2, document3));
     }
 }
